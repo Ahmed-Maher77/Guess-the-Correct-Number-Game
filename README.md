4d35d576b4a4c45b5d868fabe0ea12e9ade5b3bc
Ahmed Maher Algohary <ahmedmaheraljwhry057@gmail.com>
2024-10-25 15:58:30 +0300

    Added the main files of the project

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# Guess-the-Correct-Number-Game
-An interactive game based on a website where users guess a number between 0 and 10. The app validates input and provides instant feedback on whether the guess is correct, higher, or lower than the computer's choice.
+# Guess the Correct Number Game
+This interactive website invites users to guess a number between 0 and 10. The objective is simple: try to match the computer's randomly generated number. The game offers immediate feedback on each guess, indicating whether the user’s guess is correct, higher, or lower than the computer’s choice. Designed with a user-friendly interface and engaging animations, this application is built using modern web technologies to enhance the user experience.
+
+
+## Technologies Used
+- **HTML**
+- **CSS**
+- **TypeScript**
+- **JavaScript**
+- **Bootstrap**
+- **Font Awesome**
+
+
+## Demo (Live Preview)
+Play the game live: <a href="" title="try the game" target="_blank">here</a>
+
+
+## Key Features
+1. **Interactive Gameplay**: Users can enter a number and receive instant feedback on their guesses.
+2. **Input Validation**: Ensures only numbers within the specified range (0-10) are accepted.
+3. **Dynamic Feedback**: Pop-up messages inform the user whether they guessed correctly or if their guess was too high or too low.
+4. **Responsive Design**: Fully responsive for various devices and screen sizes.
+5. **Accessibility Features**: Incorporates ARIA attributes and semantic HTML for better accessibility.
+
+
+## Installation
+-**Set Up Node Environment:**
+=> Ensure you have Node.js and npm (Node Package Manager) installed on your computer. You can download it from Node.js official website.
+-**Download the Project:**
+=> git clone https://github.com/Ahmed-Maher77/Guess-the-Correct-Number-Game.git
+-**Navigate to the Project Folder:**
+=> cd Guess-the-Correct-Number-Game
+-**Install Dependencies:**
+=> npm install
+-**Open in Browser:**
+=> open `index.html` in your browser [by double-clicking on it]
+
---
+++ dist/index.js
@@ -0,0 +1,55 @@
+"use strict";
+// Select DOM elements
+const checkBtn = document.getElementById("checkBtn");
+const input = document.getElementById("input");
+const msgsContainer = document.querySelector(".msgs");
+const popupTrigger = document.getElementById('popup-trigger');
+const popupMsg = document.querySelector('#staticBackdrop .modal-body');
+// when click "check" button
+checkBtn === null || checkBtn === void 0 ? void 0 : checkBtn.addEventListener("click", () => {
+    // Validate input: Check for empty value
+    if (input.value.trim() === "") {
+        createMsg("Enter a value before clicking the button");
+    }
+    else {
+        const enteredNum = Number(input.value);
+        // Validate input: Check if number is valid and within range
+        if (isNaN(enteredNum) || enteredNum < 0 || enteredNum > 10) {
+            createMsg("Enter a number between 0 and 10");
+        }
+        else {
+            compareWithComputerChoice(enteredNum);
+        }
+    }
+});
+// Function to compare user's number with a random number
+function compareWithComputerChoice(enteredNum) {
+    // Generate a random number between 0 and 10
+    const randomNum = Math.trunc(Math.random() * 11);
+    // Check if the user's guess is correct
+    if (randomNum === enteredNum) {
+        createPopup('<span class="text-success">Spot On</span> <i class="fa-solid fa-circle-check text-success mt-1"></i>', `Your Choice <b>${enteredNum}</b> is correct`);
+    }
+    else {
+        const relation = enteredNum > randomNum ? 'higher' : 'lower';
+        createPopup('<span class="text-danger">Sorry, you lost!</span> <i class="fa-solid fa-circle-xmark text-danger mt-1"></i>', `You entered the number <b>${enteredNum}</b> which is ${relation} than the computer choice <b>${randomNum}</b>`);
+    }
+}
+// Function to display messages to the user
+function createMsg(msgContent) {
+    msgsContainer.innerText = msgContent;
+    msgsContainer.classList.remove('invisible');
+}
+// Hide message when input changes
+input.onchange = () => {
+    msgsContainer.classList.add('invisible');
+};
+// Function to create and display a popup
+function createPopup(head, content) {
+    popupMsg.innerHTML = `
+        <h3 class="mb-5 fw-semibold d-flex gap-2 align-items-center justify-content-center">${head}</h3>
+        <p class="fs-5 mb-0">${content}</p>
+    `;
+    popupTrigger.click(); // Trigger popup modal
+    input.value = ''; // Clear input after showing popup
+}
